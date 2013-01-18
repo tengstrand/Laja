c5f557538983af11c01ef41bdea0fe42f32acbce
Joakim Tengstrand <joakim.tengstrand@gmail.com>
2013-01-18 06:56:35 +0100

    Replace Entity + Value with Behaviour (partial commit).

--- src/example/java-gen/cdd/main/net/sf/laja/example/repository/behaviour/domain/CustomerFactory.java
+++ src/example/java-gen/cdd/main/net/sf/laja/example/repository/behaviour/domain/CustomerFactory.java
@@ -1,14 +1,76 @@
 package net.sf.laja.example.repository.behaviour.domain;
 
+import net.sf.laja.example.repository.state.*;
+import net.sf.laja.example.repository.behaviour.persistence.CustomerMatcher;
 import net.sf.laja.example.repository.state.CustomerState;
-import net.sf.laja.example.repository.state.CustomerStateBuilder;
 
-public class CustomerFactory extends CustomerEntityFactory {
+public class CustomerFactory extends CustomerBehaviour {
+
     public CustomerFactory(CustomerState state) {
         super(state);
     }
 
     public CustomerFactory(CustomerState state, CustomerStateBuilder stateBuilder) {
         super(state, stateBuilder);
+    }
+
+    public static CustomerCreator.GivenName_ ssn(long ssn) {
+        return new CustomerCreator().new Ssn_().ssn(ssn);
+    }
+
+    public static CustomerBuilder build() {
+        return new CustomerBuilder();
+    }
+
+    public static CustomerListEncapsulator createList(CustomerEncapsulator... encapsulators) {
+        return new CustomerListEncapsulator(encapsulators);
+    }
+
+    public static class CustomerFactory_ implements CustomerStateBehaviourFactory {
+        private final CustomerStateBuilder stateBuilder;
+
+        public CustomerFactory_(CustomerStateBuilder stateBuilder) {
+            this.stateBuilder = stateBuilder;
+        }
+
+        public Object create(CustomerState state, Object... args) {
+            Object result = create_(state, args);
+
+            if (!state.isValidAsEncapsulated()) {
+                throw new IllegalStateException("Illegal state, could not create behaviour class 'Customer'");
+            }
+            state.encapsulate();
+
+            return result;
+        }
+
+        private Object create_(CustomerState state, Object... args) {
+            return new Customer(state);
+
+        }
+    }
+
+    public static class CustomerMatcherFactory_ implements CustomerStateBehaviourFactory {
+        private final CustomerStateBuilder stateBuilder;
+
+        public CustomerMatcherFactory_(CustomerStateBuilder stateBuilder) {
+            this.stateBuilder = stateBuilder;
+        }
+
+        public Object create(CustomerState state, Object... args) {
+            Object result = create_(state, args);
+
+            if (!state.isValidAsEncapsulated()) {
+                throw new IllegalStateException("Illegal state, could not create behaviour class 'CustomerMatcher'");
+            }
+            state.encapsulate();
+
+            return result;
+        }
+
+        private Object create_(CustomerState state, Object... args) {
+            return new CustomerMatcher(state);
+
+        }
     }
 }
