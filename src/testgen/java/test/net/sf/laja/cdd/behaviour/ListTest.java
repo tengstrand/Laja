18f46bdbc4c74349c3898d7f55c82f5afc77e254
Joakim Tengstrand <joakim.tengstrand@gmail.com>
2013-02-02 06:03:55 +0100

    Made some cleanup.

--- src/testgen/java/test/net/sf/laja/cdd/behaviour/ListTest.java
+++ src/testgen/java/test/net/sf/laja/cdd/behaviour/ListTest.java
@@ -4,6 +4,7 @@ import net.sf.laja.cdd.behaviour.arm.Arm;
 import net.sf.laja.cdd.behaviour.brow.Brow;
 import net.sf.laja.cdd.behaviour.hand.Hand;
 import net.sf.laja.cdd.behaviour.hand.HandArrayList;
+import net.sf.laja.cdd.behaviour.hand.HandEncapsulator;
 import net.sf.laja.cdd.behaviour.hand.HandList;
 import org.junit.Test;
 
@@ -40,12 +41,20 @@ public class ListTest {
         assertEquals(Arrays.asList(1, 3), list.areas());
     }
 
+    private TestBrowEncapsulator area(int area) {
+        return TestBrow.area(area);
+    }
+
+    private TestBrow brow(int area) {
+        return area(area).asTestBrow();
+    }
+
     @Test
     public void remove_createdFromList() {
         List<TestBrow> brows = new ArrayList<TestBrow>();
-        brows.add(TestBrow.area(1).asTestBrow());
-        brows.add(TestBrow.area(2).asTestBrow());
-        brows.add(TestBrow.area(3).asTestBrow());
+        brows.add(brow(1));
+        brows.add(brow(2));
+        brows.add(brow(3));
 
         AreaBrowList list = new AreaBrowList(brows);
 
@@ -60,10 +69,7 @@ public class ListTest {
 
         AreaBrowList list = new AreaBrowList(forehead.removeBrow(1));
 
-        TestBrowList expectedList = TestBrow.createList(
-                TestBrow.area(1),
-                TestBrow.area(3)
-        ).asTestBrowList();
+        TestBrowList expectedList = TestBrow.createList(area(1),area(3)).asTestBrowList();
 
         assertEquals(expectedList, list);
         assertEquals(Arrays.asList(1, 3), list.areas());
@@ -86,55 +92,45 @@ public class ListTest {
 
     @Test
     public void contains() {
-        TestBrow testBrow = TestBrow.area(2).asTestBrow();
-        assertTrue(createBrowList().contains(testBrow));
+        assertTrue(createBrowList().contains(brow(2)));
     }
 
     @Test
     public void contains_not() {
-        TestBrow testBrow = TestBrow.area(4).asTestBrow();
-        assertFalse(createBrowList().contains(testBrow));
+        assertFalse(createBrowList().contains(brow(4)));
     }
 
     @Test
     public void iterator() {
         Iterator<TestBrow> iterator = createBrowList().iterator();
-        TestBrow firstElement = TestBrow.area(1).asTestBrow();
-
-        assertEquals(firstElement, iterator.next());
+        assertEquals(brow(1), iterator.next());
     }
 
     @Test
     public void containsAll() {
         AreaBrowList list = createBrowList();
-        AreaBrowList collection = new AreaBrowList(
-                TestBrow.area(2).asTestBrow(),
-                TestBrow.area(3).asTestBrow()
-        );
+        AreaBrowList collection = new AreaBrowList(brow(2), brow(3));
         assertTrue(list.containsAll(collection));
     }
 
     @Test
     public void get() {
         AreaBrowList list = createBrowList();
-        TestBrow expectedBrow = TestBrow.area(2).asTestBrow();
-
-        assertEquals(expectedBrow, list.get(1));
+        assertEquals(brow(2), list.get(1));
         assertEquals(new Integer(2), list.areas().get(1));
     }
 
-    private TestMouthList createTestMouthList() {
-        return TestMouthFactory.createList(TestMouth.width(10).height(5).color("Red"),
-                TestMouth.width(8).height(4).color("Blue")).asTestMouthList();
+    private HandEncapsulator hand(int area) {
+        return Hand.area(area);
     }
 
     @Test
     public void add() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1)).asArm();
-        arm1.getHands().add(Hand.area(2).asHand());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1)).asArm();
+        arm1.getHands().add(hand(2).asHand());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(2)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(2)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -142,11 +138,11 @@ public class ListTest {
 
     @Test
     public void addAtIndex() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2)).asArm();
-        arm1.getHands().add(1, Hand.area(3).asHand());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2)).asArm();
+        arm1.getHands().add(1, hand(3).asHand());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(3), Hand.area(2)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(3), hand(2)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -154,11 +150,11 @@ public class ListTest {
 
     @Test
     public void addAll() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2)).asArm();
-        arm1.getHands().addAll(Hand.createList(Hand.area(3), Hand.area(4)).asHandList());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2)).asArm();
+        arm1.getHands().addAll(Hand.createList(hand(3), hand(4)).asHandList());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(2), Hand.area(3), Hand.area(4)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(2), hand(3), hand(4)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -166,11 +162,11 @@ public class ListTest {
 
     @Test
     public void addAllAtIndex() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2)).asArm();
-        arm1.getHands().addAll(1, Hand.createList(Hand.area(3), Hand.area(4)).asHandList());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2)).asArm();
+        arm1.getHands().addAll(1, Hand.createList(hand(3), hand(4)).asHandList());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(3), Hand.area(4), Hand.area(2)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(3), hand(4), hand(2)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -178,11 +174,11 @@ public class ListTest {
 
     @Test
     public void remove() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3)).asArm();
-        arm1.removeHand(Hand.area(2).asHand());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3)).asArm();
+        arm1.removeHand(hand(2).asHand());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(3)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(3)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -190,11 +186,11 @@ public class ListTest {
 
     @Test
     public void removeAll() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3), Hand.area(4), Hand.area(5)).asArm();
-        arm1.getHands().removeAll(Hand.createList(Hand.area(3), Hand.area(4)).asHandList());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3), hand(4), hand(5)).asArm();
+        arm1.getHands().removeAll(Hand.createList(hand(3), hand(4)).asHandList());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(2), Hand.area(5)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(2), hand(5)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -202,11 +198,11 @@ public class ListTest {
 
     @Test
     public void retainAll() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3)).asArm();
-        arm1.getHands().retainAll(Hand.createList(Hand.area(2), Hand.area(3), Hand.area(4)).asHandList());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3)).asArm();
+        arm1.getHands().retainAll(Hand.createList(hand(2), hand(3), hand(4)).asHandList());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(2), Hand.area(3)).asHandList();
+        HandList expectedList = Hand.createList(hand(2), hand(3)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -214,7 +210,7 @@ public class ListTest {
 
     @Test
     public void clear() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3)).asArm();
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3)).asArm();
         arm1.getHands().clear();
         Arm arm2 = arm1.asArm();
 
@@ -226,11 +222,11 @@ public class ListTest {
 
     @Test
     public void set() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3)).asArm();
-        arm1.getHands().set(1, Hand.area(4).asHand());
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3)).asArm();
+        arm1.getHands().set(1, hand(4).asHand());
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(4), Hand.area(3)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(4), hand(3)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -238,11 +234,11 @@ public class ListTest {
 
     @Test
         public void removeAtIndex() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(1), Hand.area(2), Hand.area(3)).asArm();
+        Arm arm1 = Arm.armWeight(10).hands(hand(1), hand(2), hand(3)).asArm();
         arm1.getHands().remove(1);
         Arm arm2 = arm1.asArm();
 
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(3)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(3)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
@@ -250,13 +246,13 @@ public class ListTest {
 
     @Test
     public void sort() {
-        Arm arm1 = Arm.armWeight(10).hands(Hand.area(3), Hand.area(2), Hand.area(1)).asArm();
+        Arm arm1 = Arm.armWeight(10).hands(hand(3), hand(2), hand(1)).asArm();
         HandList hands = arm1.getHands();
 
         Collections.sort(hands);
 
         Arm arm2 = arm1.asArm();
-        HandList expectedList = Hand.createList(Hand.area(1), Hand.area(2), Hand.area(3)).asHandList();
+        HandList expectedList = Hand.createList(hand(1), hand(2), hand(3)).asHandList();
 
         assertEquals(expectedList, arm1.getHands());
         assertEquals(expectedList, arm2.getHands());
