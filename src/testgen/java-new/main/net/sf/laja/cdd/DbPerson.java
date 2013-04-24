21fac9916163565f9f37b52f40b4512e41a3b84b
Joakim Tengstrand <joakim.tengstrand@gmail.com>
2013-04-24 10:10:45 +0200

    Removed comment.

--- src/testgen/java-new/main/net/sf/laja/cdd/DbPerson.java
+++ src/testgen/java-new/main/net/sf/laja/cdd/DbPerson.java
@@ -9,16 +9,6 @@ public class DbPerson {
         this.state = state;
     }
 
-    /*
-    public final String name;
-    public final DateMidnight birthday;
-    public final ImmutableList<PersonState> children;
-    public final AddressState address;
-    public final ImmutableSet<AddressState> oldAddresses; // (optional)
-    public final ImmutableMap<String,AddressState> groupedAddresses; // (optional)
-
-     */
-
     public String getInsertStatement() {
         // Use prepared statements...
         return "insert into person (name, birthday, address) values('" + state.name + "', ...)";
