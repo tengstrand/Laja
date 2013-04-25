a771e0ab277058ee799d340df60b525d201f5dbd
Joakim Tengstrand <joakim.tengstrand@gmail.com>
2013-04-25 20:44:57 +0200

    Added withMethods().

--- src/testgen/java-new/main/net/sf/laja/cdd/state/XState.java
+++ src/testgen/java-new/main/net/sf/laja/cdd/state/XState.java
@@ -1,19 +1,15 @@
 package net.sf.laja.cdd.state;
 
-import java.util.Set;
-import java.util.List;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.joda.time.DateMidnight;
 
 public class XState {
-    private int age;
-    private String name;
-    private String hobby;   // (optional)
-    String aaaaa;
-    String bbbbbb;
-    int cccccc;
-    int ddddddd;
-    int eeeeeeee;
-    int ffff;
-    int gggg;
-    int hhhh;
-    int iiii;
+    public String name;
+    public DateMidnight birthday;
+    public ImmutableList<PersonState> children;
+    public AddressState address;
+    public ImmutableSet<AddressState> oldAddresses; // (optional)
+    public ImmutableMap<String,AddressState> groupedAddresses; // (optional)
 }
