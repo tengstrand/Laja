6f70ded1de484335bd4ba2ae6dc9925ed2d3e32a
Joakim Tengstrand <joakim.tengstrand@gmail.com>
2013-05-01 13:55:39 +0200

    Implement validation interface.

--- src/testgen/java-new/main/net/sf/laja/cdd/MutableState.java
+++ src/testgen/java-new/main/net/sf/laja/cdd/MutableState.java
@@ -4,4 +4,7 @@ import java.io.Serializable;
 
 public interface MutableState extends Serializable {
     ImmutableState asImmutable();
+    boolean isValid();
+    ValidationErrors validate();
+    void validate(String parent, ValidationErrors.Builder errors);
 }
