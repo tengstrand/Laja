c08f5ea533519db308746cf1d3d818a74f9fe586
tengstrand <joakimtengstrand@gmail.com>
2013-08-07 07:33:48 +0200

    Added an assert.

--- src/example/java/cdd/main/net/sf/laja/example/car/VehicleSize.java
+++ src/example/java/cdd/main/net/sf/laja/example/car/VehicleSize.java
@@ -3,18 +3,22 @@ package net.sf.laja.example.car;
 import net.sf.laja.example.car.state.VehicleSizeState;
 
 import static net.sf.laja.example.car.VehicleSizeCreator.VehicleSizeBehaviour;
+import static net.sf.laja.example.car.state.VehicleSizeState.LENGTH_IN_CENTIMETERS;
 
 public class VehicleSize extends VehicleSizeBehaviour {
-    public VehicleSize(VehicleSizeState state) {
-        super(state);
+
+    public VehicleSize(int lengthInCentimeters) {
+        super(new VehicleSizeState(lengthInCentimeters));
+
+        assertThat(lengthInCentimeters >= 0, LENGTH_IN_CENTIMETERS, "Length must be a positive value");
     }
 
     public boolean isSmall() {
-        return state.getLengthInCentimeters() < 300;
+        return state.lengthInCentimeters < 300;
     }
 
     public boolean isBig() {
-        return state.getLengthInCentimeters() >= 400;
+        return state.lengthInCentimeters >= 400;
     }
 
     public boolean isNormalSize() {
