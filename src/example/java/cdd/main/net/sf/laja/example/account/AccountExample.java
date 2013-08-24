d6f9b073092154305ecaafdd7ecd8c36bac0e5e7
tengstrand <joakimtengstrand@gmail.com>
2013-08-24 08:13:35 +0200

    Changed documentation to Data, Context and Interaction.

--- src/example/java/cdd/main/net/sf/laja/example/account/AccountExample.java
+++ src/example/java/cdd/main/net/sf/laja/example/account/AccountExample.java
@@ -8,7 +8,7 @@ public class AccountExample {
      * Shows how to transfer money from a source account to a destination account.
      *
      * The "transfer money" example is commonly used to explain the DCI pattern
-     * (Data Context Interaction) created by Trygve Reenskaug.
+     * (Data, Context and Interaction) created by Trygve Reenskaug.
      *
      * Note that this is CDD (Context Driven Development) created by Joakim Tengstrand.
      * CDD does not use roles, only state, behaviour and creators.
